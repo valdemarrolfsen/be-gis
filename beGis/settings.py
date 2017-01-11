3c3fdd0524c0748fa74af1e77d050def2f694131
Valdemar Rolfsen <valdemar.rolfsen@gmail.com>
2017-01-11 12:17:12 +0100

    Changed cors settings

--- beGis/settings.py
+++ beGis/settings.py
@@ -162,7 +162,7 @@ CHANNEL_LAYERS = {
 # CORS
 CORS_ORIGIN_WHITELIST = (
         'localhost:3000',
-        'http://begis-web.s3-website-eu-west-1.amazonaws.com'
+        'begis-web.s3-website-eu-west-1.amazonaws.com'
     )
 
 CORS_ALLOW_CREDENTIALS = True
